db13ff3de2ce46aa4e4fa71bd4444ba88a64044b
GAGGAH1 <titusdcstd2022@gmail.com>
2025-09-11 14:01:42 +0100

    Added URI for the DB

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 const express = require('express');
+
+require('dotenv').config();
+
+const connectDB = require("./config/db")
+
+const app  = express();
+const PORT = process.env.PORT
+
+
+
+
+connectDB.then(() => {
+    app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+    });
+});
+
+
---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("MONGODB CONNECTED SUCCESSFULLY")
+    } catch (error) {
+        console.error("MONGODB CONNECTION FAILED", error);  
+        process.exit(1);
+    }
+};
